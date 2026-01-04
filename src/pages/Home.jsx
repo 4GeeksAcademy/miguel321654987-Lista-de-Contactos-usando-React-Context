4f71c8854eb1c52844c56989b51345da42fbbc95
Miguel Angel Lopez Hermida <lacarricanta@hotmail.com>
2026-01-04 21:48:25 +0000

    Commit 04-01-2026

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,64 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import { useEffect } from "react";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { useState } from "react";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+    const { store, dispatch } = useGlobalReducer()
+    const [listado, setListado] = useState([])
+    console.log(listado)
+    const slug = "miguel321654987"
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-		</div>
-	);
+
+    useEffect(() => {
+        // 1. Intentamos ver si la agenda existe
+        const inicializarAgenda = async () => await fetch(`https://playground.4geeks.com/contact/agendas/${slug}`)
+            .then(response => {
+                if (response.status === 404) {
+                    // 2. Si NO existe (404), la CREAMOS primero
+                    return fetch(`https://playground.4geeks.com/contact/agendas/${slug}`, {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" }
+                    })
+
+                        .then(() => {
+                            // 3. Una vez creada la agenda, CREAMOS el contacto
+                            return fetch(`https://playground.4geeks.com/contact/agendas/${slug}/contacts`, {
+                                method: "POST",
+                                body: JSON.stringify({
+                                    name: "...............",
+                                    phone: "000000000",
+                                    email: "@example.com",
+                                    address: "........"
+                                }),
+                                headers: { "Content-Type": "application/json" }
+                            });
+                        });
+                }
+                // Si la agenda ya existía, simplemente pedimos los contactos
+                return fetch(`https://playground.4geeks.com/contact/agendas/${slug}/contacts`);
+            })
+            .then(response => response.json())
+            .then(data => {
+                // Seteamos el listado (manejando si viene de la creación o de la consulta)
+                setListado(data.contacts || [data]);
+            })
+            .catch(error => console.error("Error en el proceso:", error));
+    
+
+    inicializarAgenda();
+}, [slug]);
+
+
+
+return (
+    <div className="text-center mt-5">
+        <div>
+            <h1>
+                LISTA DE CONTACTOS
+            </h1>
+        </div>
+
+    </div>
+);
 }; 
